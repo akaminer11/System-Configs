668634c81f3203ea13cc3b5197c488fcbcfe36a4
bruhmomentum <54458370+akaminer11@users.noreply.github.com>
2022-01-10 14:13:57 -0500

    Delete install.sh

--- scripts/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# install WM and dependencies
-
-pacman -S xorg xorg-xinit nitrogen picom nvidia nvidia-utils
-
-# install tools necessary for base use
-
-pacman -S terminator pcmanfm lxappearance firefox git
